9503fec8232db3b8b108729cf3ff06cd87311557
test <che.kt@mail.ru>
2016-05-21 20:08:45 +0700

    Initial commit

---
+++ app/src/main/java/com/lazytest/airo/lazytest/PreviewFragment.java
@@ -0,0 +1,20 @@
+package com.lazytest.airo.lazytest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Airo on 21.05.2016.
+ */
+public class PreviewFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_preview, container, false);
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/lazytest/airo/lazytest/StrongFragment.java
@@ -0,0 +1,9 @@
+package com.lazytest.airo.lazytest;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Airo on 21.05.2016.
+ */
+public class StrongFragment extends Fragment {
+}
